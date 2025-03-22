69066a547cf5d0fc5b72aaab10411cd47e1d9c34
Boburjon <shadow88bob@gmail.com>
2025-03-22 21:56:16 +0500

    Add Navbar and update code

---
+++ src/components/Navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <nav class="bg-blue-600 p-4 text-white flex justify-between">
+        <h1 class="text-xl font-bold">AI Resume Builder</h1>
+        <div>
+            <router-link to="/" class="mr-4 hover:underline">Home</router-link>
+            <router-link to="/dashboard" class="mr-4 hover:underline">Dashboard</router-link>
+            <router-link to="/auth" class="hover:underline">Login</router-link>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style></style>
